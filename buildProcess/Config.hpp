d258f9940a46fa0768f79eafaf5f16d72fa463db
Andre Jarboe <andrejarboe@gmail.com>
2026-01-06 15:18:12 -0600

    Refactor project structure: move Config and main files to source directory and update .gitignore

--- buildProcess/Config.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-float jumpHeight = 50.0f;
-float jumpDuration = 0.5f;
-
-
---
+++ buildProcess/source/Config.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+    defined variables
+    ther wil be storage made form them in memory 
+*/
+float jumpHeight = 50.0f;
+float jumpDuration = 0.5f;
+
+
